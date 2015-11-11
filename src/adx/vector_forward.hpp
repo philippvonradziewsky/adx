463eab0cff478024f0320093a7a950288cd8ed0d
Philipp Harms <philipp.harms@googlemail.com>
2015-11-11 21:40:15 +0100

    Add forward declaration

---
+++ src/adx/vector_forward.hpp
@@ -0,0 +1,28 @@
+/*
+ * This file is part of adx.
+ *
+ * adx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * adx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with adx.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ADX_VECTOR_FORWARD_HPP_
+#define ADX_VECTOR_FORWARD_HPP_
+
+namespace adx {
+
+template<typename T, std::size_t Extent>
+class vector;
+
+}
+
+#endif /* end of include guard: ADX_VECTOR_FORWARD_HPP_ */
